00af3d8a35d539bf64c5f56b1f530efea985bc3b
Kurhan Artem <kurhanartem@MacBook-Air-uzivatela-Kurhan.local>
2026-01-21 23:01:00 +0100

    Finished

--- app/main.py
+++ app/main.py
@@ -1,15 +1,15 @@
 class Animal:
     alive = []
 
-    def __init__(self, name: str, hidden: bool = False,
-                 health: int = 100) -> None:
+    def __init__(self, name: str, health: int = 100,
+                 hidden: bool = False) -> None:
         self.name = name
         self.hidden = hidden
         self.health = health
         Animal.alive.append(self)
 
     def cheak_alive(self) -> None:
-        if self.health <= 0:
+        if self.health <= 0 and self in Animal.alive:
             Animal.alive.remove(self)
 
     def __repr__(self) -> str:
